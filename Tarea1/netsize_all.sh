3b37ec9c44b17e749a5e548188022a74cb75d628
RuthGuevara <95975746+RuthGuevara@users.noreply.github.com>
2021-12-11 19:50:39 -0500

    Update netsize_all.sh
    Resultados a

--- Tarea1/netsize_all.sh
+++ Tarea1/netsize_all.sh
@@ -1,1 +1,6 @@
-
+#Para realizar el ejercicio 2
+- Necesitamos abrir el directorio de la cartepa Saavedra2013
+- Con el terminal Cat n1.txt se imprime el contenido de Saavedra2013
+- Se ejecuta el codigo git bush para archivo en $ ( ls * .txt ) ; hacer wc -l $ archivo ; head -n1 $ archivo | grep -o " " | wc -l ; hecho
+- Finalmente se obtienen todos los valores de fila y colunma para los archivos n59.txt
+## Resultados en netsize_all.txt
