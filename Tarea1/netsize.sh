6d109b815d79ab72923e36d105e7ab0f61e762cd
RuthGuevara <95975746+RuthGuevara@users.noreply.github.com>
2021-12-11 19:41:09 -0500

    Update netsize.sh
    Resultados

--- Tarea1/netsize.sh
+++ Tarea1/netsize.sh
@@ -1,1 +1,11 @@
-
+#Para realizar el ejercicio 1
+- Necesitamos abrir GitBash.
+- Ubicamos la ruta de nuestro computador en donde estè la carpeta Saavedra2013 utilizando el codigo "cd Saavedra"
+- Con el terminal Cat n1.txt se imprime el contenido de Saavedra2013
+- Se obtiene el numero de filas y columnas con el codigo echo "El numero de colunmas es: " ; head -n1 n1.txt | grep -o " " | wc -l ; echo " El numero de filas es: " ; wc -l n1.txt ; echo " Fin de la ejecucion "
+## Como resultado final obtenemos
+### El numero de colunmas es:
+80
+### El numero de filas es:
+97
+### Fin de Ejecuciòn 
